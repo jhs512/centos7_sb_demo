bcc254f8b1be120c5c49efefd427d7ecfbc0a28b
jhs512 <jangka512@gmail.com>
2020-09-09 12:43:48 +0900

    세션을 레디스로 관리

--- src/main/java/com/example/sbs/demo/controller/usr/HomeController.java
+++ src/main/java/com/example/sbs/demo/controller/usr/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.sbs.demo.controller.usr;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,20 @@ public class HomeController {
 	@RequestMapping("/")
 	@ResponseBody
 	public String showMain() {
-		return articleDao.getAllCount() + "개!!";
+		return articleDao.getAllCount() + "개 입니다.";
+	}
+
+	@RequestMapping("/doSetSessionA")
+	@ResponseBody
+	public String doSetSessionA(HttpSession session) {
+		session.setAttribute("a", 10);
+
+		return "세션설정완료";
+	}
+
+	@RequestMapping("/getSessionA")
+	@ResponseBody
+	public int getSessionA(HttpSession session) {
+		return (int) session.getAttribute("a");
 	}
 }
